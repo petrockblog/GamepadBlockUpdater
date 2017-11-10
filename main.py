8f92cc6324f542e8a79d230c866a37f568a93e15
petrockblog <contact@petrockblock.com>
2017-11-10 20:47:00 +0100

    Finished updater GUI

--- main.py
+++ main.py
@@ -16,40 +16,52 @@ class Application(tk.Frame):
 
         self.serialPort = tk.StringVar()
         self.extractedVersion = ""
-        self.title = ttk.Label(self, text="GamepadBlock Firmware Updater")
-        self.title.grid(row=0, column=0, columnspan=2, pady=5, padx=5)
 
-        self.recentFirmwareVersion = ttk.Label(self, text="")
-        self.recentFirmwareVersion.grid(row=1, column=0, pady=5, padx=5)
+        self.recentFirmwareVersion = tk.Label(self, text="", relief="sunken", justify="center")
+        self.recentFirmwareVersion.grid(row=1, column=0, pady=5, padx=5, columnspan=2, sticky=tk.E+tk.W)
 
-        self.step_1_label = ttk.Label(self, text="Step 1: Connect the GamepadBlock to your PC")
+        self.step_1_label = tk.Label(self, text="Step 1: Connect the GamepadBlock to your PC")
         self.step_1_label.grid(row=2, column=0, columnspan=2, sticky='W', pady=5, padx=5)
 
-        self.step_3_label = ttk.Label(self, text="Step 3: Select the COM port of the GamepadBlock")
+        self.step_3_label = tk.Label(self, text="Step 3: Select the COM port of the GamepadBlock")
         self.step_3_label.grid(row=4, column=0, sticky="W", pady=5, padx=5)
 
-        self.step_3_cbox = ttk.Combobox(self, textvariable=self.serialPort, value=self.serial_ports())
-        self.step_3_cbox.grid(row=4, column=1, pady=5, padx=5)
+        self.step_3_cbox = ttk.Combobox(self, textvariable=self.serialPort, value=self.serial_ports(), state="readonly")
+        self.step_3_cbox.grid(row=4, column=1, pady=5, padx=5, sticky=tk.E+tk.W)
         self.step_3_cbox.bind("<<ComboboxSelected>>", self.readVersion)
 
-        self.step_2_label = ttk.Label(self, text="Step 2: Refresh the list of COM ports").grid(row=3, column=0, sticky="W", pady=5, padx=5)
-        self.step_2_button = ttk.Button(self, text="Refresh", command=self.refreshSerialPorts).grid(row=3, column=1, pady=5, padx=5)
+        self.step_2_label = tk.Label(self, text="Step 2: Refresh the list of COM ports").grid(row=3, column=0, sticky="W", pady=5, padx=5)
+        self.step_2_button = tk.Button(self, text="Refresh", command=self.refreshSerialPorts).grid(row=3, column=1, pady=5, padx=5, sticky=tk.E+tk.W)
 
-        self.versionLabel = ttk.Label(self, text="")
-        self.versionLabel.grid(row=5, column=0, sticky="W", pady=5, padx=5)
+        self.versionLabel = tk.Label(self, text="", relief="sunken", justify="center")
+        self.versionLabel.grid(row=5, column=0, pady=5, padx=5, columnspan=2, sticky=tk.E+tk.W)
 
-        self.step_4_label = ttk.Label(self, text="Step 4: Press the Reset button on the GamepadBlock").grid(row=6, column=0, columnspan=2, sticky="W", pady=5, padx=5)
-        self.step_5_label = ttk.Label(self, text="Step 5: Download the new firmware to the GamepadBlock").grid(row=7, column=0, sticky="W", pady=5, padx=5)
-        self.step_5_button = ttk.Button(self, text="Start Download", command=self.downloadFirmware).grid(row=7, column=1, pady=5, padx=5)
+        self.step_4_label = tk.Label(self, text="Step 4: Press the Reset button on the GamepadBlock").grid(row=6, column=0, columnspan=2, sticky="W", pady=5, padx=5)
+        self.step_4_button = tk.Button(self, text="Ok, I have pressed the reset button", command=self.resetPressed, state="disabled")
+        self.step_4_button.grid(row=6, column=1, pady=5, padx=5, sticky=tk.E+tk.W)
+
+        self.step_5_label = tk.Label(self, text="Step 5: Download the new firmware to the GamepadBlock").grid(row=7, column=0, sticky="W", pady=5, padx=5)
+        self.step_5_button = tk.Button(self, text="Start Download", command=self.downloadFirmware, state="disabled")
+        self.step_5_button.grid(row=7, column=1, pady=5, padx=5, sticky=tk.E+tk.W)
+
+        self.downloadInfo = tk.Label(self, text="", relief="sunken", justify="center")
+        self.downloadInfo.grid(row=8, column=0, pady=5, padx=5, columnspan=2, sticky=tk.E+tk.W)
+
+        self.infoLabel = tk.Label(self, text="gamepadblock.petrockblock.com").grid(row=10, column=0, sticky="W", pady=5, padx=5)
+        self.exitButton = tk.Button(self, text="Quit", command=self.quit).grid(row=10, column=1, pady=5, padx=5, sticky=tk.E)
+
         self.pack()
 
         r = requests.get("https://github.com/petrockblog/GamepadBlockUpdater/releases/latest")
         downloadURL = r.url
         self.currentVersion = self.findVersionString(downloadURL)
-        self.recentFirmwareVersion['text'] = "Most recent firmware version is " + self.currentVersion
+        self.recentFirmwareVersion['text'] = "INFO: The most recent firmware version is " + self.currentVersion
         downloadURL = downloadURL.replace("releases/tag", "releases/download")
         downloadURL += "/firmware.hex"
         self.downloadFile(downloadURL, "currentFirmware.hex")
+
+    def resetPressed(self):
+        self.step_5_button['state'] = "normal"
 
     def refreshSerialPorts(self):
         self.step_3_cbox['values'] = self.serial_ports()
@@ -80,13 +92,22 @@ class Application(tk.Frame):
         if self.extractedVersion is not None:
             self.extractedVersion = self.extractedVersion.group(1)
             print("Found version " + self.extractedVersion)
-            self.versionLabel['text'] = "Found GamepadBlock. The firmware version of it is  " + self.extractedVersion
+            self.versionLabel['text'] = "INFO: Found GamepadBlock. The firmware version of it is  " + self.extractedVersion
+            self.step_4_button['state'] = "active"
         else:
             print("Did not find any GamepadBlock")
-            self.versionLabel['text'] = "Did not find GamepadBlock."
+            self.versionLabel['text'] = "INFO: Did not find GamepadBlock. Please check concection or try another COM port."
+            self.step_4_button['state'] = "disabled"
+            self.step_5_button['state'] = "disabled"
 
     def downloadFirmware(self):
-        call(["avrdude","-p","m32u2","-c","avr109","-P",self.serialPort.get(),"-u","-U","flash:w:currentFirmware.hex:a"])
+        self.downloadInfo['text'] = "Started update process ..."
+        self.update()
+        returnValue = call(["avrdude","-p","m32u2","-c","avr109","-P",self.serialPort.get(),"-u","-U","flash:w:currentFirmware.hex:a"])
+        if returnValue == 0:
+            self.downloadInfo['text'] = "Update finished successfully."
+        else:
+            self.downloadInfo['text'] = "Update was not successful (error code " + str(returnValue) + ")."
 
     def serial_ports(self):
         """ Lists serial port names
@@ -117,5 +138,6 @@ class Application(tk.Frame):
         return result
 
 root = tk.Tk()
+root.resizable(width=False, height=False)
 app = Application(master=root)
 app.mainloop()
